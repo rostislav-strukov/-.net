b79d1844fb48e10fb6f38f661a7c188546dcc977
rostiks <strukovra1@gmail.com>
2016-07-28 23:44:10 +0300

    пока черновик

---
+++ Hwork 6/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppScaut
+{
+    class Program
+    {
+        private static List<Scout> OurScouts;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------------------------------------------------Please choose you enter-----------------------------------------------------------------");
+            Console.WriteLine("| Add scout enter 1 | Delete scout enter 2 | Add sport enter 3 | Delete sport enter 4 | Add aword scout enter 5 | Delete aword scout enter 6 | Show all scout enter 7 |");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+            Console.ReadLine();
+
+            SelectItems();
+            CreateScout();
+
+            Console.ReadKey();
+        }
+
+        public static void SelectItems()
+        {
+            char _iEnter = (char)Console.ReadKey().Key;
+
+            switch (_iEnter) 
+            {
+                case '1': // добавить скаута
+                    break;
+                case '2': // удалить скаута
+                    break;
+                case '3': // добавить спорт 
+                    break;
+                case '4': // удалить спорт
+                    break;
+                case '5': // добавить награду
+                    break;
+                case '6': // удалить награду
+                    break;
+                case '7': // показать всех
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void CreateScout()
+        {
+            Scout scout;
+
+            Console.WriteLine("Please enter sex of scout print in line Male - m or Female - f: ");
+
+            char _mySex = Console.ReadLine()[0];
+
+            switch (_mySex)
+            {
+                case 'm':
+                    Console.WriteLine("Enter name for scout: ");
+                    scout = new BoyScout(Console.ReadLine());
+                    break;
+                case 'f':
+                    Console.WriteLine("Enter name for scout: ");
+                    scout = new GirlScout(Console.ReadLine());
+                    break;
+                default:
+                    Console.WriteLine("Wrong enter");
+                    break;
+            }
+        }
+    }
+
+    class Scout
+    {
+        private string _iName;
+        private string _iSex;
+        
+        public string Name { get { return _iName; }  set { _iName = value; } }
+        public string Sex { get { return _iSex; } set { _iSex = value; } }
+    }
+
+    class BoyScout : Scout
+    {
+        public BoyScout(string _iName) : base(_iName)
+        {
+   
+        }
+    }
+
+    class GirlScout : Scout
+    {
+        public GirlScout(string _iName) : base(_iName)
+        {
+
+        }
+    }
+
+    class Sport
+    {
+
+    }
+
+    class SportGirl
+    {
+
+    }
+
+    class SportBoy
+    {
+
+    }
+}
