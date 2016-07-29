d55bf619830969ccd580dc3df0bd2fc83aceb622
rostiks <strukovra1@gmail.com>
2016-07-30 01:16:49 +0300

    create Scout

--- Hwork 6/Program.cs
+++ Hwork 6/Program.cs
@@ -4,126 +4,324 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AppScaut
+namespace ConsoleAppScaut
 {
     class Program
     {
         private static List<Scout> Scouts;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("-------------------------------------------------------------------------------Please choose you enter-----------------------------------------------------------------");
-            Console.WriteLine("| Add scout enter 1 | Delete scout enter 2 | Add sport enter 3 | Delete sport enter 4 | Add aword scout enter 5 | Delete aword scout enter 6 | Show all scout enter 7 |");
-            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-            Console.ReadLine();
+            Scouts = new List<Scout>();
 
-            SelectItems();
-            CreateScout();
-
-            Console.ReadKey();
+            ShowMenu();
+            SelectMenuItem();
         }
-
-        public static void SelectItems()
-        {
-            char _iEnter = (char)Console.ReadKey().Key;
-
-            switch (_iEnter) 
-            {
-                case '1': // добавить скаута
-                    break;
-                case '2': // удалить скаута
-                    break;
-                case '3': // добавить спорт 
-                    break;
-                case '4': // удалить спорт
-                    break;
-                case '5': // добавить награду
-                    break;
-                case '6': // удалить награду
-                    break;
-                case '7': // показать всех
-                    break;
-                default:
-                    break;
-            }
-        }
-
         private static void CreateScout()
         {
             Scout _iScout;
+            Console.Write("Please choose sex of scout Male - m or Female - f:");
 
-            Console.WriteLine("Please enter sex of scout print in line Male - m or Female - f: ");
-
-            char _iSex = Console.ReadLine()[0];
-
-            switch (_iSex)
+            char sex = Console.ReadLine()[0];
+            switch (sex)
             {
-                case 'm':
-                    Console.WriteLine("Enter name for scout: ");
-                    _iScout = new BoyScout(Console.ReadLine());
-                    break;
                 case 'f':
-                    Console.WriteLine("Enter name for scout: ");
+                    Console.Write("Enter name of scout:");
                     _iScout = new GirlScout(Console.ReadLine());
                     break;
+                case 'm':
+                    Console.Write("Enter name of scout:");
+                    _iScout = new BoyScout(Console.ReadLine());
+                    break;
                 default:
-                    Console.WriteLine("Wrong enter");
+                    Console.WriteLine("Wrong char");
                     return;
             }
             Scouts.Add(_iScout);
-            Console.WriteLine($"Scout { _iScout.Name} added");
+            Console.WriteLine($"New Scout '{_iScout.Name}' added.");
+            Console.ReadKey();
+        }
+
+        private static void DeleteScout()
+        {
+            int _iCheck = 0;
+
+            foreach (Scout i in Scouts)
+            {
+                Console.WriteLine($"({_iCheck}) {i.Name}");
+                _iCheck++;
+            }
+
+            Console.Write("Please choose scout in list:");
+
+            int _iIndex = -1;
+
+            if (int.TryParse(Console.ReadLine(), out _iIndex) && _iIndex <= Scouts.Count - 1)
+                Scouts.RemoveAt(_iIndex);
+            else
+            {
+                Console.WriteLine("Wrong index!");
+                return;
+            }
         }
 
         private static void AddSport()
         {
+            int _iCheck = 0;
+
+            foreach (Scout i in Scouts)
+            {
+                Console.WriteLine($"({_iCheck}) {i.Name}");
+                _iCheck++;
+            }
+
+            Console.Write("Please choose scout in the list");
+
+            int _iIndex = -1;
+
+            if (int.TryParse(Console.ReadLine(), out _iIndex) && _iIndex <= Scouts.Count - 1)
+                Console.WriteLine($"You choosed scout {Scouts[_iIndex].Name} ");
+            else
+            {
+                Console.WriteLine("Wrong index!");
+                return;
+            }
+            Scouts[_iIndex].AddSport();
+        }
+        private static void AddAward()
+        {
+            int _iCheck = 0;
+
+            foreach (Scout i in Scouts)
+            {
+                Console.WriteLine($"({_iCheck}) {i.Name}");
+                _iCheck++;
+            }
+
+            Console.Write("Please choose scout in list:");
+
+            int _iIndex = -1;
+
+            if (int.TryParse(Console.ReadLine(), out _iIndex) && _iIndex <= Scouts.Count - 1)
+                Console.WriteLine($"You choosed scout {Scouts[_iIndex].Name} ");
+            else
+            {
+                Console.WriteLine("Wrong index!");
+                return;
+            }
+            ShowMenu();
+            Scouts[_iIndex].AddProgress();
+        }
+        private static void DeleteSport()
+        {
+            int _iCheck = 0;
+
+            foreach (Scout i in Scouts)
+            {
+                Console.WriteLine($"({_iCheck}) {i.Name}");
+                _iCheck++;
+            }
+
+            Console.Write("Please choose scout in list:");
+
+            int _iIndex = -1;
+
+            if (int.TryParse(Console.ReadLine(), out _iIndex) && _iIndex <= Scouts.Count - 1)
+                Console.WriteLine($"You choosed scout {Scouts[_iIndex].Name} ");
+            else
+            {
+                Console.WriteLine("Wrong index!");
+                return;
+            }
+            Scouts[_iIndex].DeleteSport();
+        }
+        private static void DeleteAward()
+        {
+            int _iCheck = 0;
+
+            foreach (Scout i in Scouts)
+            {
+                Console.WriteLine($"({_iCheck}) {i.Name}");
+                _iCheck++;
+            }
+
+            Console.Write("Please choose scout in list:");
+
+            int _iIndex = -1;
+
+            if (int.TryParse(Console.ReadLine(), out _iIndex) && _iIndex <= Scouts.Count - 1)
+                Console.WriteLine($"You choosed scout {Scouts[_iIndex].Name} ");
+            else
+            {
+                Console.WriteLine("Wrong index!");
+                return;
+            }
+            ShowMenu();
+            Scouts[_iIndex].DeleteAward();
+        }
+
+        private static void ShowAll()
+        {
+            foreach (Scout i in Scouts)
+            {
+                Console.WriteLine($"{ i.Name}({i.MySex})");
+                foreach (Scout.Award j in i.GetProgress)
+                {
+                    Console.WriteLine($"Scout {j.Name} : {j.Points}");
+                }
+            }
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+        private static void ShowMenu()
+        {
+            Console.WriteLine("-------------------------------------------------------------------------------Please choose you enter-----------------------------------------------------------------");
+            Console.WriteLine("| Add scout enter 1 | Delete scout enter 2 | Add sport enter 3 | Delete sport enter 4 | Add aword scout enter 5 | Delete aword scout enter 6 | Show all scout enter 7 |");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        }
+        private static void SelectMenuItem()
+        {
+            char _iKey = Console.ReadLine()[0];
+
+            Console.WriteLine();
+            switch (_iKey)
+            {
+                case '1':  // добавить скаута
+                    CreateScout();
+                    break;
+                case '2': // удалить скаута
+                    DeleteScout();
+                    break;
+                case '3': // добавить спорт
+                    AddSport();
+                    break;
+                case '4': // удалить спорт
+                    DeleteSport();
+                    break;
+                case '5': // добавить награду
+                    AddAward();
+                    break;
+                case '6': // удалить награду
+                    DeleteAward();
+                    break;
+                case '7': // показать всех
+                    ShowAll();
+                    break;
+            }
+        }
+    }
+    abstract class Scout
+    {
+
+        private List<Award> Progress;
+        private List<Sport> Sports;
+        private string _iName;
+        protected Sex _iSex;
+
+
+        public string Name { get { return _iName; } set { _iName = value; } }
+        public List<Award> GetProgress { get { return Progress; } }
+        public Sex MySex { get { return _iSex; } }
+
+        public List<Sport> GetSport { get { return Sports; } }
+
+        public Scout(string _iName)
+        {
+            this._iName = _iName;
+            Progress = new List<Award>();
+        }
+        public void AddProgress()
+        {
+            Progress = new List<Award>();
+            Award NewAward = new Award();
+
+            Console.Write("Please enter award: ");
+            NewAward.Name = Console.ReadLine();
+
+            Console.Write("Please enter score: ");
+            NewAward.Points = Convert.ToByte(Console.ReadLine());
+
+            Progress.Add(NewAward);
 
         }
 
+        public void AddSport()
+        {
+            Sports = new List<Sport>();
+            Sport NewSport = new Sport();
 
+            Console.Write("Please enter Sport: ");
+            NewSport.GetSport = Console.ReadLine();
+            Sports.Add(NewSport);
+        }
+        public void DeleteSport()
+        {
+            int _iCheck = 0;
+
+            foreach (Sport i in Sports)
+            {
+                Console.WriteLine($"({_iCheck}) {Sports[_iCheck].GetSport}");
+                _iCheck++;
+            }
+            Console.WriteLine("Please choose sport in the list:");
+            int _iIndex = -1;
+
+            if (int.TryParse(Console.ReadLine(), out _iIndex) && _iIndex <= Sports.Count - 1)
+            {
+                Console.WriteLine($"Delete {Sports[_iIndex].GetSport}");
+                Sports.RemoveAt(_iIndex);
+            }
+            else
+            {
+                Console.WriteLine("Wrong index!");
+                return;
+            }
+        }
+
+        public void DeleteAward()
+        {
+            int _iCheck = 0;
+            foreach (Award i in Progress)
+            {
+                Console.WriteLine($"({_iCheck}) {Progress[_iCheck].Name}");
+                _iCheck++;
+            }
+            Console.Write("Please choose award in the list:");
+            int _iIndex = -1;
+
+            if (int.TryParse(Console.ReadLine(), out _iIndex) && _iIndex <= Progress.Count - 1)
+            {
+                Console.WriteLine($"Delete {Progress[_iIndex].Name}");
+                Progress.RemoveAt(_iIndex);
+            }
+            else
+            {
+                Console.WriteLine("Wrong index!");
+                return;
+            }
+        }
+        public class Sport
+        {
+            private string _iSport;
+
+            public string GetSport { get { return _iSport; } set { _iSport = value; } }
+        }
+        public class Award
+        {
+            private string _iName;
+            private int _iPoints;
+            public string Name { get { return _iName; } set { _iName = value; } }
+            public int Points { get { return _iPoints; } set { _iPoints = value; } }
+        }
     }
-
-    class Scout
+    class GirlScout : Scout
     {
-        private string _iName;
-        protected Sex _mySex;
-
-        public string Name { get { return _iName; } set { _iName = value; } }
-
-        public Sex MySex { get { return _mySex; } }
+        public GirlScout(string _iName) : base(_iName) { _iSex = Sex.Female; }
     }
     class BoyScout : Scout
     {
-        public BoyScout(string _iName) : base(_iName)
-        {
-            _mySex = Sex._iMale; ;
-        }
+        public BoyScout(string name) : base(name) { _iSex = Sex.Male; }
     }
-
-    class GirlScout : Scout
-    {
-        public GirlScout(string _iName) : base(_iName)
-        {
-             _mySex = Sex._iFemale;
-        }
-    }
-
-    class Sport
-    {
-
-    }
-
-    class SportGirl
-    {
-
-    }
-
-    class SportBoy
-    {
-
-    }
-
     enum Sex
-    {
-        _iMale = 'm',
-        _iFemale = 'f'
-    }
+    { Male = 'm', Female = 'f' }
 }
